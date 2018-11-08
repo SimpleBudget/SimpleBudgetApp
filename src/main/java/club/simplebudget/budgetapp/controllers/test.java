70c053bddf6623eb9bfb337ade6d7a5dc476cae9
patrickrfisher <patrick.ryan.fisher@gmail.com>
2018-11-07 18:47:58 -0600

    completed a partial which holds the navbar,footer,and header

--- src/main/java/club/simplebudget/budgetapp/controllers/test.java
+++ src/main/java/club/simplebudget/budgetapp/controllers/test.java
@@ -1,4 +1,10 @@
 package club.simplebudget.budgetapp.controllers;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class test {
+    @GetMapping("/")
+    public String Landing(){
+        return "homepage";
+    }
 }
