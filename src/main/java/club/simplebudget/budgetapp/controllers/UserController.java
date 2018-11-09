cc8cce529c2adcc6bb431e4368a9f1f9bbff9e15
patrickrfisher <patrick.ryan.fisher@gmail.com>
2018-11-09 15:52:49 -0600

    edit profile and profile security

--- src/main/java/club/simplebudget/budgetapp/controllers/UserController.java
+++ src/main/java/club/simplebudget/budgetapp/controllers/UserController.java
@@ -2,12 +2,11 @@ package club.simplebudget.budgetapp.controllers;
 
 import club.simplebudget.budgetapp.models.User;
 import club.simplebudget.budgetapp.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
@@ -31,5 +30,26 @@ public class UserController {
         user.setPassword(hash);
         users.save(user);
         return "redirect:/login";
+    }
+    @GetMapping("/users/{id}/edit")
+    public String editProfile(@PathVariable Long id, Model vModel){
+        vModel.addAttribute("user",users.findOne(id));
+        return "users/edit";
+    }
+    @PostMapping("/users/{id}/edit")
+    public String updateProfile(@ModelAttribute User user, @PathVariable Long id, @RequestParam String newpassword,@RequestParam String confirmpassword){
+        User loggedInUser =(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(users.findOne(id).getId() == loggedInUser.getId() && !newpassword.equals("")&& confirmpassword.equals(newpassword)) {
+            String hash = passwordEncoder.encode(newpassword);
+            user.setPassword(hash);
+            users.save(user);
+            return "redirect:/profile";
+        }else if(users.findOne(id).getId() == loggedInUser.getId() && newpassword.equals("")){
+            user.setPassword(loggedInUser.getPassword());
+            users.save(user);
+            return "redirect:/profile";
+        } else {
+            return "redirect:/";
+        }
     }
 }
