939929f82a48c51102ee39610005c2606d929ab7
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-11-22 00:55:47 -0600

    Still need to make pie chart mobile responsive, but it's looking nice and it's dynamic

--- src/main/resources/static/js/Chart.js
+++ src/main/resources/static/js/Chart.js
@@ -4,23 +4,48 @@ let spendAmount = document.getElementById("spendAmount");
 let amount = (moneyAfterBillsNumber/28);
 spendAmount.innerText = "You can spend $" + amount.toFixed(2) + " per day.";
 let billTotal = document.getElementById("billTotal");
+let billNames = Array.from(document.getElementsByClassName("billName"));
+let billAmounts = Array.from(document.getElementsByClassName("billAmount"));
+let monthlyIncome = parseFloat(document.getElementById("monthlyIncome").innerText.substring(1,document.getElementById("monthlyIncome").innerText.length-1));
 
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+
+
+dataArray = [
+    ['Monthly Income', 'Expenses'],
+    ['Left over after bills', moneyAfterBillsNumber],
+
+]
+billNames.forEach((element,index)=>{
+    dataArray.push([billNames[index].innerText.charAt(0).toUpperCase() + billNames[index].innerText.slice(1), Number(billAmounts[index].innerText)],)
+})
 
 function drawChart() {
 
-    var data = google.visualization.arrayToDataTable([
-        ['Monthly Income', 'Expenses'],
-        ['Total of bills', Number(billTotal.innerText.substring(1,billTotal.innerText.length-1))],
-        ['Daily Spending', moneyAfterBillsNumber]
-    ]);
+    var data = google.visualization.arrayToDataTable(dataArray);
 
     var options = {
-        title: 'Monthly Income Broken Down'
+        width: $("#piechartsize").width() - $("#piechartsize").width()*.26,
+        height: $("#piechartsize").height(),
+        title: 'Budget Broken Down',
+        is3D: true
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-    chart.draw(data, options);
+    chart.draw(data,options);
+
+    function resizeChart () {
+        chart.draw(data, options);
+    }
+    if (document.addEventListener) {
+        document.getElementById("piechartsize").addEventListener('resize', resizeChart);
+    }
+    else if (document.attachEvent) {
+        document.getElementById("piechartsize").attachEvent('onresize', resizeChart);
+    }
+    else {
+        document.getElementById("piechartsize").resize = resizeChart;
+    }
 }
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
